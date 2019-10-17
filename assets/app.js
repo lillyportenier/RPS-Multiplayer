e27675016f934befa2d170a51dbbd9f23f55dd2d
laportenier <lilly.portenier@gmail.com>
2019-10-17 15:52:15 -0400

    addign firebase

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+var Config = {
+    apiKey: "AIzaSyBg1n4rtFKz8SYsXwItzCJef9WLGuMmN14",
+    authDomain: "rps-multiplayer-4652d.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-4652d.firebaseio.com",
+    projectId: "rps-multiplayer-4652d",
+    storageBucket: "rps-multiplayer-4652d.appspot.com",
+    messagingSenderId: "817137673519",
+    appId: "1:817137673519:web:0af067d226ff3c6af4adda"
+  };
+
+  firebase.initializeApp(Config);
+
+  var database = firebase.database();
+
+
